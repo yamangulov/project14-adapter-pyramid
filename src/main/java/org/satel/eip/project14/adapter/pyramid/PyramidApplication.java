b5649eb7f66ebdcc8089ed48a4e4f89b3e081831
Ямангулов Андрей <yamangulov@satel.org>
2021-11-26 15:22:10 +0300

    Initial commit

---
+++ src/main/java/org/satel/eip/project14/adapter/pyramid/PyramidApplication.java
@@ -0,0 +1,13 @@
+package org.satel.eip.project14.adapter.pyramid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyramidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyramidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/satel/eip/project14/adapter/pyramid/PyramidApplicationTests.java
@@ -0,0 +1,13 @@
+package org.satel.eip.project14.adapter.pyramid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyramidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
